1d2b6ba039b1c7dc6831ea80304bde44bd027209
Chuuuqs <nwachuku24@gmail.com>
2026-03-30 19:54:01 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const maths = require('./maths')
+
+// function greet(name){
+//     return "hello " + name
+
+// }
+//     console.log(greet("chuuqs"));
+
+// // Function Expression
+// const greet1 = function(name){
+//     return "hello " + name
+// }
+//     console.log(greet1("Eme"));
+
+// // Arrow function
+
+// const greet2 = (name) => {
+//     return "hello " + name
+// }
+//     console.log(greet2("Emnez"));
+
+console.log(`${maths.add(5, 3)} and ${maths.sub(5,3)}`)
